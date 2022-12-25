49fd755315711a79cde1183eb93c040e10e8fbde
husna <hpm25@georgetown.edu>
2022-12-24 16:12:56 -0800

    fix first comment

--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 class Knight
   attr_accessor :position, :parent
 
-  # [i, j] i = rows, j = columns of board 
+  # [i, j] i = columns, j = rows of board 
   # from the current position, it has 8 possible paths
-  # ex: [i + 2, j + 1], [i + 1, j + 2]...[i + 2, j - 1] 
+  # ex: [i + 1, j + 2], [i - 2, j - 1]...[i + 2, j + 1] 
   COORDINATES = [[1, 2], [-2, -1], [-1, 2], [2, -1], [1, -2], [-2, 1], [-1, -2], [2, 1]];
 
   def initialize(position, parent = nil)
